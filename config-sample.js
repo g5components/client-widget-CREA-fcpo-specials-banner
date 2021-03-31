ff7071494b337976ccb23270008eb0aa437d32f4
Adam Sworob <adam.sworob@getg5.com>
2021-03-31 03:52:38 -0400

    Add CMS CLI

---
+++ config-sample.js
@@ -0,0 +1,13 @@
+const config = {
+  widget: {
+    id: 4429539,
+    amount_of_categories: {
+      value_or_default: '2'
+    }
+  },
+  filters: {
+    darken: (x) => x
+  }
+}
+
+module.exports = { config }
